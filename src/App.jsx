dbf21514fdaef71d14360edd8dc484b2683fd280
Vaishnavi Soni <vaishnavisoni72@gmail.com>
2024-10-07 17:00:46 +0530

    :zap: rename file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React,{useState} from 'react';
+
 import './App.css';
 
 function App() {
   let [Name,setName]= useState("vaishnavi")
   return (
    <div>
-    <h1>hello {Name}</h1>
+    <h1>hello {Name} jvlkxcjl</h1>
    </div>
   );
 }
-
+    
 export default App;
